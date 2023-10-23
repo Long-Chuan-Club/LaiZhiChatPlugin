d509b1948abf799ef901a68775aaf69e043513da
huvz <dachen04@outlook.com>
2023-10-23 21:12:27 +0800

    :desktop_computer:添加skia依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
     id("net.mamoe.mirai-console") version "2.15.0"
 
 }
-
+val kotlin_Version = "1.8.20"
+val kotlinx_coroutines = "1.7.3"
 group = "LaiZhi.Chat.Repeater"
 version = "0.2.2"
 repositories {
@@ -15,6 +16,16 @@ repositories {
 dependencies {
     compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.7.1")
     implementation("io.ktor:ktor-client-okhttp:2.2.4")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-swing
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-swing:${kotlinx_coroutines}")
+
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.80")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinx_coroutines}")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlin_Version}")
+// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib-jdk8
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlin_Version}")
 }
 mirai {
     jvmTarget = JavaVersion.VERSION_17
